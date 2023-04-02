2639967b6abc53abeae253980e83cace2c3f8f7c
alliums-p <107186732+alliums-p@users.noreply.github.com>
2023-04-02 18:19:35 +0545

    creating auth and encrypting password while user signups

---
+++ server/routes/auth.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { signup } from "../controllers/auth.js";
+
+const router = express.Router();
+
+// Create a user
+router.post("/signup", signup);
+
+// Sign In
+// router.post("/signin", )
+
+// Google Auth
+// router.post("/google-signup", )
+
+export default router;

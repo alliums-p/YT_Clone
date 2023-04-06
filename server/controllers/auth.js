0955328c38cc32e37071e5a2d29f2ccb29dcafb3
alliums-p <107186732+alliums-p@users.noreply.github.com>
2023-04-06 18:11:37 +0545

    refactoring the code and preventing password to be sent along with the access token during signin

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,42 +1,66 @@
 import mongoose from "mongoose";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
-import { createError } from "../error.js";
 import jwt from "jsonwebtoken";
+import { createError } from "../error.js";
 
 export const signup = async (req, res, next) => {
   try {
+    // Validate input
+    const { username, password } = req.body;
+    if (!username || !password) {
+      return next(createError(400, "Username and password are required!"));
+    }
+
+    // Hash password and create new user
     const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(req.body.password, salt);
+    const hash = bcrypt.hashSync(password, salt);
     const newUser = new User({
-      ...req.body,
+      username,
       password: hash,
     });
 
     await newUser.save();
     res.status(200).json(newUser);
   } catch (error) {
+    // Handle expected errors
+    if (error.code === 11000) {
+      return next(createError(400, "Username already taken!"));
+    }
     next(error);
   }
 };
 
 export const signin = async (req, res, next) => {
   try {
-    const user = await User.findOne({ username: req.body.username });
-    if (!user) return next(createError(404, "User not found!"));
+    // Validate input
+    const { username, password } = req.body;
+    if (!username || !password) {
+      return next(createError(400, "Username and password are required!"));
+    }
 
-    const isCorrect = await bcrypt.compare(req.body.password, user.password);
+    // Find user by username
+    const user = await User.findOne({ username });
+    if (!user) {
+      return next(createError(404, "User not found!"));
+    }
 
-    if (!isCorrect) return next(createError(404, "Wrong user credentials!"));
+    // Compare password with hash
+    const isCorrect = await bcrypt.compare(password, user.password);
+    if (!isCorrect) {
+      return next(createError(400, "Wrong password!"));
+    }
 
+    // Sign and return JWT token
     const token = jwt.sign({ id: user._id }, process.env.JWT);
+    const { password: _, ...others } = user._doc;
 
     res
       .cookie("access_token", token, {
         httpOnly: true,
       })
       .status(200)
-      .json(user);
+      .json(others);
   } catch (error) {
     next(error);
   }

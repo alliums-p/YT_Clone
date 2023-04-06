e9148cb2555b855cf3d83423c6f251cacf6199eb
alliums-p <107186732+alliums-p@users.noreply.github.com>
2023-04-06 21:14:05 +0545

    creating a verification function to verify token and creating rest of the users routes and controller functions

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,3 +1,86 @@
+import { createError } from "../error.js";
+import User from "../models/User.js";
+
 export const test = (req, res) => {
   res.json("Successful br");
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.params.id === req.user.id) {
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      return res.status(200).json(updatedUser);
+    } catch (err) {
+      next(err);
+    }
+  } else {
+    return next(createError(403, "No authorization for update!"));
+  }
+};
+export const deleteUser = async (req, res, next) => {
+  if (req.params.id === req.user.id) {
+    try {
+      await User.findByIdAndDelete(req.params.id, {
+        $set: req.body,
+      });
+      return res.status(200).json("User has been deleted!");
+    } catch (err) {
+      next(err);
+    }
+  } else {
+    return next(createError(403, "No authorization to delete the account!!"));
+  }
+};
+
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    return res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
+export const subscribe = async (req, res, next) => {
+  try {
+    await User.findById(req.user.id, {
+      $push: { subscribedUsers: req.params.id },
+    });
+    await User.findByIdAndUpdate(req.params.id, {
+      $inc: { subscribers: 1 },
+    });
+    res.status(200).json("Subscription successful!");
+  } catch (error) {
+    next(error);
+  }
+};
+export const unsubscribe = async (req, res, next) => {
+  try {
+    await User.findById(req.user.id, {
+      $pull: { subscribedUsers: req.params.id },
+    });
+    await User.findByIdAndUpdate(req.params.id, {
+      $inc: { subscribers: -1 },
+    });
+    res.status(200).json("Successful unsubscribed!");
+  } catch (error) {
+    next(error);
+  }
+};
+export const like = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+export const dislike = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
 };
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,36 @@
 import express from "express";
-import { test } from "../controllers/user.js";
+import {
+  deleteUser,
+  dislike,
+  getUser,
+  like,
+  subscribe,
+  unsubscribe,
+  updateUser,
+} from "../controllers/user.js";
+import { verifyTokken } from "../verifyToken.js";
 
 const router = express.Router();
 
-router.get("/test", test);
+// update user
+router.put("/:id", verifyTokken, updateUser);
+
+// delete user
+router.delete("/:id", verifyTokken, deleteUser);
+
+// get a user
+router.get("/find/:id", getUser);
+
+// subscribe a user
+router.put("/subscribe/:id", verifyTokken, subscribe);
+
+// unsubscribe a user
+router.put("/unsubscribe/:id", verifyTokken, unsubscribe);
+
+// like a video
+router.put("/like/:id", verifyTokken, like);
+
+// dislike a video
+router.put("/dislike/:id", verifyTokken, dislike);
 
 export default router;
